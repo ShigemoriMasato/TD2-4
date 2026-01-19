21d5a0b61d58fccbbf7a4745c3d93bf376c6bab5
Shigemori <k024g0027@g.neec.ac.jp>
2026-01-20 01:48:45 +0900

    途中

--- DirectXGame/Engine/Assets/Font/FontLoader.cpp
+++ DirectXGame/Engine/Assets/Font/FontLoader.cpp
@@ -1,68 +1,131 @@
 #include "FontLoader.h"
 
 #include <algorithm>
+#include <cassert>
 
 void FontLoader::Initialize() {
-	FT_Init_FreeType(&ft_);
+	Logger logger = getLogger("Engine");
+	logger->info("FontLoader Initialize");
+
+	FT_Error err = FT_Init_FreeType(&ft_);
+	if (err) {
+		logger->error("Failed to initialize FreeType Library");
+		assert(false);
+		return;
+	}
 }
 
 int FontLoader::Load(const std::string& filePath, int fontSize) {
-	FT_Face face = nullptr;
-
 	//stringの整合性チェック
-	std::string factPath{};
-	
+	std::string factPath = FilePathChecker(filePath);
+	std::string cacheFileName = filePath + "_" + std::to_string(fontSize);
 
-	FT_New_Face(ft_, factPath.c_str(), 0, &face);
-	FT_Set_Pixel_Sizes(face, 0, fontSize);
-
-	int penX = 0;
-	int maxTop = 0;
-	uint32_t maxBottom = 0;
-
-	for (char c : text_) {
-		FT_Load_Char(face, c, FT_LOAD_RENDER);
-		auto& g = face->glyph;
-
-		penX += g->advance.x >> 6;
-
-		maxTop = std::max(maxTop, g->bitmap_top);
-		maxBottom = std::max(maxBottom, g->bitmap.rows - g->bitmap_top);
+	if (fontIndex_.find(cacheFileName) != fontIndex_.end()) {
+		return fontIndex_[cacheFileName];
 	}
 
-	int width = penX;
-	int height = maxTop + maxBottom;
+	logger_->info("FontLoader Load: {}", factPath);
 
-	std::vector<uint8_t> image(width * height * 4, 0);
+	auto values = binaryManager_.Read(cachePath_ + cacheFileName);
+	IntermediateFontData fontBuffer;
 
-	penX = 0;
-	int baseline = maxTop;
+	if (values.empty()) {
+		fontBuffer = CreateFontBuffer(factPath, fontSize);
 
-	for (char c : text_) {
+
+	} else {
+		int index = 0;
+
+		//AtlasData
+		int size = BinaryManager::Reverse<int>(values[index++].get());
+		fontBuffer.atlasData.resize(atlas_width_ * atlas_height_, 0);
+		for (int i = 0; i < size; ++i) {
+			fontBuffer.atlasData[i] = BinaryManager::Reverse<uint8_t>(values[index++].get());
+		}
+
+		//CharPositions
+	}
+
+
+
+	return 0;
+}
+
+FontLoader::IntermediateFontData FontLoader::CreateFontBuffer(const std::string& filePath, int fontSize) {
+	IntermediateFontData data{};
+	data.atlasData.resize(atlas_width_ * atlas_height_, 0);
+
+	FT_Face face = nullptr;
+
+	FT_New_Face(ft_, filePath.c_str(), 0, &face);
+	FT_Set_Pixel_Sizes(face, 0, fontSize);
+
+	//文字の最大の高さを取得
+	int maxTop = 0;
+	int maxBottom = 0;
+
+	for (wchar_t c : text_) {
 		FT_Load_Char(face, c, FT_LOAD_RENDER);
 		auto& g = face->glyph;
-		auto& bmp = g->bitmap;
 
-		int x0 = penX + g->bitmap_left;
-		int y0 = baseline - g->bitmap_top;
+		maxTop = std::max(maxTop, g->bitmap_top);
+		maxBottom = std::max(maxBottom, (int)g->bitmap.rows - g->bitmap_top);
+	}
 
-		for (uint32_t y = 0; y < bmp.rows; y++) {
-			for (uint32_t x = 0; x < bmp.width; x++) {
-				uint8_t a = bmp.buffer[y * bmp.pitch + x];
-				if (a == 0) continue;
+	int height = maxTop + maxBottom;
 
-				int dst = ((y0 + y) * width + (x0 + x)) * 4;
-				image[dst + 0] = 255;
-				image[dst + 1] = 255;
-				image[dst + 2] = 255;
-				image[dst + 3] = a;
+	//文字を配置する位置
+	int dest_x = 0;
+	int dest_y = 0;
+
+	for (const wchar_t& c : text_) {
+		//フォントの文字を持ってくる
+		FT_Load_Char(face, c, FT_LOAD_RENDER);
+
+		auto& bitmap = face->glyph->bitmap;
+
+		//改行処理
+		if (dest_x + (int)bitmap.width >= atlas_width_) {
+			dest_x = 0;
+			dest_y += height;
+
+			//Atlasの高さを超えたら終了
+			if (dest_y + height >= atlas_height_) {
+				break;
 			}
 		}
 
-		penX += g->advance.x >> 6;
+		for (int y = 0; y < (int)bitmap.rows; ++y) {
+			for (int x = 0; x < (int)bitmap.width; ++x) {
+				uint8_t alpha = bitmap.buffer[y * bitmap.pitch + x];
+				if (alpha == 0) continue;
+
+				//下線を揃えるためにmaxTopを基準にしてyをオフセットさせる
+				int offsetY = maxTop - face->glyph->bitmap_top;
+				int targetIndex = (dest_y + offsetY + y) * atlas_width_ + (dest_x + x);
+
+				data.atlasData[targetIndex] = alpha;
+				data.lastUsedIndex = std::max(data.lastUsedIndex, targetIndex);
+
+				//UV座標を登録
+				CharPosition charPos;
+				charPos.uvStart = Vector2(
+					static_cast<float>(dest_x) / static_cast<float>(atlas_width_),
+					static_cast<float>(dest_y + offsetY) / static_cast<float>(atlas_height_)
+				);
+				charPos.uvEnd = Vector2(
+					static_cast<float>(dest_x + bitmap.width) / static_cast<float>(atlas_width_),
+					static_cast<float>(dest_y + offsetY + bitmap.rows) / static_cast<float>(atlas_height_)
+				);
+				data.charPositions[c] = charPos;
+			}
+		}
+
+		//次の文字の位置を更新
+		dest_x += bitmap.width + 2;
 	}
 
-	return 0;
+	return data;
 }
 
 std::string FontLoader::FilePathChecker(const std::string& filePath) {
@@ -87,4 +150,50 @@ std::string FontLoader::FilePathChecker(const std::string& filePath) {
 	}
 
 	return {};
+}
+
+void FontLoader::CreateCache(const std::string& cacheFileName, const IntermediateFontData& data) {
+
+	//AtlasData
+	binaryManager_.RegisterOutput(static_cast<int>(data.lastUsedIndex));
+	for (int i = 0; i < data.lastUsedIndex; ++i) {
+		binaryManager_.RegisterOutput(data.atlasData[i]);
+	}
+
+	//CharPositions
+	binaryManager_.RegisterOutput(static_cast<int>(data.charPositions.size()));
+	for (const auto& [key, value] : data.charPositions) {
+		binaryManager_.RegisterOutput(static_cast<int>(key));
+		binaryManager_.RegisterOutput(value.uvStart);
+		binaryManager_.RegisterOutput(value.uvEnd);
+	}
+
+	binaryManager_.Write(cachePath_ + cacheFileName);
+}
+
+void FontLoader::LoadCache(const std::string& cacheFileName, IntermediateFontData& data) {
+	auto values = binaryManager_.Read(cachePath_ + cacheFileName);
+
+	if (values.empty()) {
+		return;
+	}
+
+	int index = 0;
+	//AtlasData
+	data.lastUsedIndex = BinaryManager::Reverse<int>(values[index++].get());
+	data.atlasData.resize(atlas_width_ * atlas_height_, 0);
+	for (int i = 0; i < data.lastUsedIndex; ++i) {
+		data.atlasData[i] = BinaryManager::Reverse<uint8_t>(values[index++].get());
+	}
+	//CharPositions
+	int charPosSize = BinaryManager::Reverse<int>(values[index++].get());
+	for (int i = 0; i < charPosSize; ++i) {
+		wchar_t key = static_cast<wchar_t>(BinaryManager::Reverse<int>(values[index++].get()));
+		Vector2 uvStart = BinaryManager::Reverse<Vector2>(values[index++].get());
+		Vector2 uvEnd = BinaryManager::Reverse<Vector2>(values[index++].get());
+		CharPosition charPos;
+		charPos.uvStart = uvStart;
+		charPos.uvEnd = uvEnd;
+		data.charPositions[key] = charPos;
+	}
 }
--- DirectXGame/Engine/Assets/Font/FontLoader.h
+++ DirectXGame/Engine/Assets/Font/FontLoader.h
@@ -4,6 +4,8 @@
 #include <unordered_map>
 #include <string>
 
+#include <Tool/Binary/BinaryManager.h>
+
 class FontLoader {
 public:
 
@@ -13,12 +15,50 @@ public:
 
 private:
 
+	struct CharPosition {
+		Vector2 uvStart;
+		Vector2 uvEnd;
+	};
+
+	struct FontData {
+		int fontSize;
+		int textureIndex;
+		std::unordered_map<wchar_t, CharPosition> charPositions;
+	};
+
+	struct IntermediateFontData {
+		std::vector<uint8_t> atlasData;
+		std::unordered_map<wchar_t, CharPosition> charPositions;
+		int lastUsedIndex;
+	};
+
+private:
+
+	IntermediateFontData CreateFontBuffer(const std::string& filePath, int fontSize);
+
 	std::string FilePathChecker(const std::string& filePath);
 
-	std::vector<char> text_;
+	void CreateCache(const std::string& cacheFileName, const IntermediateFontData& data);
+	void LoadCache(const std::string& cacheFileName, IntermediateFontData& data);
 
+private://フォント関係
+	std::vector<wchar_t> text_;
 	FT_Library ft_ = nullptr;
-
 	std::unordered_map<std::string, FT_Face> fonts_;
 
+private://入出力関係
+	BinaryManager binaryManager_;
+	std::string basePath_ = "Assets/Fonts/";
+	std::string cachePath_ = "Assets/Fonts/cache/";
+
+private://アトラス関係
+	const int atlas_width_ = 2048;
+	const int atlas_height_ = 2048;
+
+private://フォントデータ
+	std::unordered_map<std::string, int> fontIndex_;
+	std::vector<FontData> fontData_;
+
+private://Debug
+	Logger logger_;
 };
