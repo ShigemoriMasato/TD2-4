1912c78095a625fe9429b0581d19ba9f9a947be4
saitoA1007 <k024g0004@g.neec.ac.jp>
2026-01-28 15:01:19 +0900

    ステージ選択画面にフォントを入れた

---
+++ DirectXGame/AppSource/SelectStageNum.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/// <summary>
+/// 選択したステージ
+/// </summary>
+class SelectStageNum {
+public:
+
+	static inline int num_ = 1;
+};
--- DirectXGame/AppSource/UI/SelectStageUI.h
+++ DirectXGame/AppSource/UI/SelectStageUI.h
@@ -1,10 +1,11 @@
 #pragma once
 #include"Object/SpriteObject.h"
+#include"Object/FontObject.h"
 
 class SelectStageUI {
 public:
 
-	void Init(DrawData drawData);
+	void Init(DrawData drawData, const std::string& fontName,DrawData fontDrawData, FontLoader* fontLoader);
 
 	void Update();
 
@@ -19,9 +20,14 @@ public:
 	}
 
 private:
+	// フォントロード
+	FontLoader* fontLoader_ = nullptr;
 
 	// 画像データ
 	std::unique_ptr<SpriteObject> spriteObject_;
+
+	// フォントデータ
+	std::unique_ptr<FontObject> fontObject_;
 
 	// アニメーションのフラグ
 	bool isAnimation_ = false;
@@ -31,6 +37,11 @@ private:
 	float timer_ = 0.0f;
 
 	bool isChange_ = false;
+
+	std::wstring name_ = L"Stage";
+
+	// フォントの位置
+	Vector3 fontPos_ =  { 497.0f,150.0f,0.0f };
 
 private: // 調整項目
 
