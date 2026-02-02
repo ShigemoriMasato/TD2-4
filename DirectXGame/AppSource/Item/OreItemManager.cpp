cc88483ed8cc4676a57e58b5360aef29bc91e5a3
Shigemori <k024g0027@g.neec.ac.jp>
2026-02-03 00:26:29 +0900

    light

--- DirectXGame/AppSource/Item/OreItemManager.cpp
+++ DirectXGame/AppSource/Item/OreItemManager.cpp
@@ -292,9 +292,9 @@ void OreItemManager::AddOreItem(OreType type, const Vector3& pos, const float& r
 	//PointLightの追加
 	PointLight light{};
 	light.color = ConvertColor(0xcfd942ff);
-	light.intensity = 3.5f;
-	light.radius = 2.0f;
-	light.decay = 1.5f;
+	light.intensity = 10.0f;
+	light.radius = 3.0f;
+	light.decay = 4.0f;
 	Vector3* position = oreItems_[index]->GetPosPtr();
 	int lightHandle = LightManager::GetInstance()->AddPointLight(position, light);
 	oreItems_[index]->RegistLightIndex(lightHandle);
