94b29fe46fb873072797dae0c321c2595377ad99
saitoA1007 <k024g0004@g.neec.ac.jp>
2026-02-04 17:08:22 +0900

    フォントを差し替えた

--- DirectXGame/AppSource/Game/GameScene.cpp
+++ DirectXGame/AppSource/Game/GameScene.cpp
@@ -380,7 +380,7 @@ void GameScene::Initialize() {
 
 	// 最初と最後のカウントをするUI
 	startCountUI_ = std::make_unique<StartCountUI>();
-	startCountUI_->Initialize(fontName, drawData, fontLoader_, commonData_->nextMapIndex, "RockFont.ttf");
+	startCountUI_->Initialize(fontName, drawData, fontLoader_, commonData_->nextMapIndex, "Kuramubon.otf");
 	startCountUI_->isStart_ = true;
 
 	// ミニマップ操作時の時間操作処理
