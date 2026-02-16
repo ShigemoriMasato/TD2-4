375c587216c782edc466f47fcc92ff46030ded1e
ShigemoriMasato <k024g0027@g.neec.ac.jp>
2026-02-16 18:07:55 +0900

    a

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,6 +1,12 @@
 #include <Core/DXDevice.h>
+#include <Core/Command/CommandManager.h>
 
 int WinMain(HINSTANCE hInstance, HINSTANCE prev, LPSTR str, int state) {
+
+	auto device = std::make_unique<SHEngine::DXDevice>();
+	device->Initialize();
+	auto cmdManager = std::make_unique<SHEngine::Command::Manager>();
+	cmdManager->Initialize(device.get());
 	
 	return 0;
 }
