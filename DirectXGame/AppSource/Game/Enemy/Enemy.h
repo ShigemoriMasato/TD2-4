d2017b449772b165aeafc1a975953608340d342b
AsakawaSakuto <k024g1017@g.neec.ac.jp>
2026-02-22 20:02:35 +0900

    ファイル作成

---
+++ DirectXGame/AppSource/Game/Enemy/Enemy.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <cstdint>
+#include <Utility/Vector.h>
+
+enum class EnemyType : int {
+	Normal,
+
+	Count,
+};
+
