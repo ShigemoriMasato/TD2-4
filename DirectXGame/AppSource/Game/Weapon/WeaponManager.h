a9942627c1e11a1c13f5bf8c9d80836bd4d1dc12
Ratta38 <k024g1030@g.neec.ac.jp>
2026-02-25 10:36:14 +0900

    ㎓

--- DirectXGame/AppSource/Game/Weapon/WeaponManager.h
+++ DirectXGame/AppSource/Game/Weapon/WeaponManager.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "WeaponData.h"
+#include <Tool/Binary/BinaryManager.h>
 
 /// <summary>
 /// 武器のパラメータ管理用クラス
@@ -13,4 +14,27 @@ public:
 
 	// IDから武器データを取得する
 	WeaponData* GetWeapon(int id);
+
+private:
+	// モデルデータをバイナリにセーブする関数
+	void SaveModel();
+
+	// モデルデータをバイナリからロードする関数
+	void LoadModel();
+
+	// 武器データをバイナリにセーブする関数
+	void SaveItem();
+
+	// 武器データをバイナリからロードする関数
+	void LoadItem();
+
+	// 武器のパラメータをバイナリにセーブする関数
+	void SaveBaseParam();
+
+	// モデルデータをバイナリにセーブする関数
+	void LoadBaseParam();
+
+private:
+	// バイナリマネージャ
+	BinaryManager binaryManager_;
 };
