b2a4210c60723b4d7fea50559e4e8dccd55d91df
Ratta38 <k024g1030@g.neec.ac.jp>
2026-03-02 16:40:25 +0900

    WeaponのデータをJsonに保存できるように変更

--- DirectXGame/AppSource/Scene/InitializeScene.cpp
+++ DirectXGame/AppSource/Scene/InitializeScene.cpp
@@ -115,8 +115,8 @@ std::unique_ptr<IScene> InitializeScene::Update() {
 	//更新処理
 	commonData_->cmdObject->ResetCommandList();
 
-	//return std::make_unique<MiharaScene>();
-	return std::make_unique<BackPackBuildScene>();
+	return std::make_unique<MiharaScene>();
+	//return std::make_unique<BackPackBuildScene>();
 }
 
 void InitializeScene::Draw() {
