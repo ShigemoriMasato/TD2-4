97c62ddeea3ea425ad12ec7aa8f0be9e4efbd2e4
AsakawaSakuto <k024g1017@g.neec.ac.jp>
2026-03-06 17:59:40 +0900

    Title用のModelを追加

--- DirectXGame/AppSource/Scene/01_Title/TitleScene.h
+++ DirectXGame/AppSource/Scene/01_Title/TitleScene.h
@@ -1,6 +1,14 @@
 #pragma once
 #include <Scene/IScene.h>
 
+namespace Title {
+	enum class State {
+		Start,
+		Options,
+		Exit,
+	};
+}
+
 class TitleScene : public IScene {
 public:
 
@@ -12,4 +20,5 @@ public:
 
 private:
 
+	Title::State state_ = Title::State::Start;
 };
