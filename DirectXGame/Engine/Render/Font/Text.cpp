09588e8bd0c0f6e860de7031ad69b1b37e75fb28
ShigemoriMasato <k024g0027@g.neec.ac.jp>
2026-03-16 17:52:24 +0900

    text

--- DirectXGame/Engine/Render/Font/Text.cpp
+++ DirectXGame/Engine/Render/Font/Text.cpp
@@ -1,1 +1,51 @@
 #include "Text.h"
+
+using namespace Matrix;
+
+void SHEngine::Text::Initialize(const std::string& fontPath, int fontSize) {
+	textureIndex_ = fontLoader_->Load(fontPath, fontSize);
+	fontPath_ = fontPath;
+	fontSize_ = fontSize;
+
+	render_ = std::make_unique<RenderObject>();
+	render_->psoConfig_.vs = "Engine/FontBasic.VS.hlsl";
+	render_->psoConfig_.ps = "Engine/FontBasic.PS.hlsl";
+	render_->CreateCBV(sizeof(Matrix4x4) * 2, ShaderType::VERTEX_SHADER, "Matrix");
+	render_->CreateSRV(sizeof(CharPosition), maxCharNum_, ShaderType::VERTEX_SHADER, "CharConfig");
+	render_->CreateCBV(sizeof(int), ShaderType::PIXEL_SHADER, "TextureIndex");
+	render_->CreateCBV(sizeof(Vector4), ShaderType::PIXEL_SHADER, "Color");
+	render_->SetUseTexture(true);
+}
+
+void SHEngine::Text::SetText(const std::wstring& text) {
+	if(text.size() > maxCharNum_) {
+		return;
+	}
+
+	charPositions_.clear();
+	for (wchar_t character : text) {
+		charPositions_.push_back(fontLoader_->GetCharPosition(fontPath_, character, fontSize_));
+	}
+}
+
+void SHEngine::Text::SetSize(float size) {
+	charSizeMat_ = MakeScaleMatrix({ size, size, 1.0f });
+}
+
+void SHEngine::Text::SetTransform(const Transform& transform) {
+	worldMat_ = MakeScaleMatrix(transform.scale) * MakeRotationMatrix(transform.rotate) * MakeTranslationMatrix(transform.position);
+}
+
+void SHEngine::Text::Update(float deltaTime, Matrix4x4 vpMat) {
+	wvpMat_ = charSizeMat_ * worldMat_ * vpMat;
+
+	Matrix4x4 mat[2] = { wvpMat_, worldMat_ };
+	render_->CopyBufferData(0, mat, sizeof(mat));
+	render_->CopyBufferData(1, charPositions_.data(), sizeof(CharPosition) * charPositions_.size());
+	render_->CopyBufferData(2, &textureIndex_, sizeof(textureIndex_));
+	render_->CopyBufferData(3, &color_, sizeof(Vector4));
+}
+
+void SHEngine::Text::Draw(Command::Object* cmdObj) {
+	render_->Draw(cmdObj);
+}
--- DirectXGame/Engine/Render/Font/Text.h
+++ DirectXGame/Engine/Render/Font/Text.h
@@ -9,23 +9,36 @@ namespace SHEngine {
 
 		static void SetFontLoader(FontLoader* fontLoader) { fontLoader_ = fontLoader; }
 
+		Text(int maxCharNum = 128) : maxCharNum_(maxCharNum) {};
+
 		void Initialize(const std::string& fontPath, int fontSize = 64);
 		void Initialize(int fontID);
 
 		void SetText(const std::wstring& text);
+		void SetSize(float size);
+		void SetTransform(const Transform& transform);
+		void SetColor(const Vector4& color) { color_ = color; }
+		void Update(float deltaTime, Matrix4x4 vpMat);
+		void Draw(Command::Object* cmdObj);
 
 	private:
 
 		static inline FontLoader* fontLoader_ = nullptr;
 
 		int textureIndex_ = -1;
+
+		std::string fontPath_ = "";
 		int fontSize_ = 0;
 
 		std::unique_ptr<RenderObject> render_ = nullptr;
 		std::vector<CharPosition> charPositions_{};
+		Vector4 color_ = { 1.0f, 1.0f, 1.0f, 1.0f };
 
 		Matrix4x4 charSizeMat_ = Matrix4x4::Identity();
+		Matrix4x4 worldMat_ = Matrix4x4::Identity();
 		Matrix4x4 wvpMat_ = Matrix4x4::Identity();
+
+		const int maxCharNum_;
 
 	};
 
