ed5cb820b4ceea4b80738cd1b822e27859624318
ShigemoriMasato <k024g0027@g.neec.ac.jp>
2026-03-25 17:34:40 +0900

    a

---
+++ DirectXGame/SHResource.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <Core/DXDevice.h>
+
+enum class BufferType : uint8_t {
+	CBV = 1 << 0,
+	SRV = 1 << 1,
+	UAV = 1 << 2,
+	RTV = 1 << 3,
+	DSV = 1 << 4,
+	ReadBack = 1 << 5,
+};
+
+namespace SHEngine {
+
+	struct ResourceDesc {
+		D3D12_RESOURCE_DESC desc;
+	};
+
+	class Resource {
+	public:
+		Resource(ID3D12Resource* resource);
+		Resource(ResourceDesc& desc)
+
+	private:
+
+	};
+
+}
+
