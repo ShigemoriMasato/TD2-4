56b9bd82b64c4143321455bc85dfd22941515183
Ratta38 <k024g1030@g.neec.ac.jp>
2026-03-29 16:47:52 +0900

    弓のモデル変更

--- DirectXGame/AppSource/GameObject/Weapon/Data/Bow.cpp
+++ DirectXGame/AppSource/GameObject/Weapon/Data/Bow.cpp
@@ -1,8 +1,17 @@
 #include "Bow.h"
+#include "GameObject/Attack/Arrow.h"
 
 void Bow::Initialize(int weaponID, Player::Base* player) {
 	IRangedWeapon::Initialize(weaponID, player);
 	config_.speed *= 0.8f;
 	speed_ = config_.speed;
 	penetration_ = config_.penetration;
+}
+
+void Bow::Shot(IEnemy* target) {
+	// 敵の方向に弾を生成
+	isAnimation_ = true;
+	std::unique_ptr<Arrow> arrow = std::make_unique<Arrow>();
+	arrow->Initialize(config_);
+	attackManager_->AddObj(std::move(arrow));
 }
