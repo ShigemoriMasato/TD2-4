c754715a83ece17f532da488ddc110f302f8512b
ShigemoriMasato <k024g0027@g.neec.ac.jp>
2026-04-16 14:25:10 +0900

    レンダーターゲット管理の強化と各種初期化処理の修正
    ・IDisplayにToRenderTargetを追加し、Single/MultiDisplayで実装。SetRenderTargetで描画前バリア遷移を明示的に実行
    ・KeyManagerのキー初期化処理をresultKeyFlags_全体に修正

--- DirectXGame/AppSource/Scene/InitializeScene.cpp
+++ DirectXGame/AppSource/Scene/InitializeScene.cpp
@@ -171,10 +171,6 @@ std::unique_ptr<IScene> InitializeScene::Update() {
 	//更新処理
 	commonData_->cmdObject->ResetCommandList();
 
-	return std::make_unique<YokoScene>();
-	return std::make_unique<PrticleEditorScene>();
-	return std::make_unique<TrailEditorScene>();
-	return std::make_unique<ShigeScene>();
 	return std::make_unique<TitleScene>();
 	return std::make_unique<TrailEditorScene>();
 	return std::make_unique<PrticleEditorScene>();
