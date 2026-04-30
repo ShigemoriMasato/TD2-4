94440b21861d5f6703d871af27f0d1eec2f5be96
Ratta38 <k024g1030@g.neec.ac.jp>
2026-04-30 18:28:51 +0900

    エフェクト追加

--- DirectXGame/Assets/Shader/Game/AfterImage.PS.hlsl
+++ DirectXGame/Assets/Shader/Game/AfterImage.PS.hlsl
@@ -1,9 +1,10 @@
-StructuredBuffer<float4> colors : register(t0);
 
 struct PSInput
 {
     float4 position : SV_POSITION;
-    uint instanceID : SV_InstanceID;
+    float2 texCoord : TEXCOORD0;
+    float3 normal : NORMAL0;
+    uint instanceID : INSTANCE0;
 };
 
 struct PSOutput
@@ -11,9 +12,27 @@ struct PSOutput
     float4 color : SV_TARGET;
 };
 
+cbuffer TextureIndex : register(b0)
+{
+    int textureIndex;
+};
+
+struct ColorBuffer
+{
+    float4 color;
+};
+StructuredBuffer<ColorBuffer> colors : register(t0);
+StructuredBuffer<int> textureIndices : register(t1);
+Texture2D<float4> textures[] : register(t8);
+SamplerState gSampler : register(s0);
+
 PSOutput main(PSInput input)
 {
     PSOutput output;
-    output.color = colors[input.instanceID];
+
+    float4 color = colors[input.instanceID].color;
+    int texIdx = textureIndices[input.instanceID];
+    float4 texColor = textures[texIdx].Sample(gSampler, input.texCoord);
+    output.color = color * texColor;
     return output;
 }
