05bcc52885327984d33fec879bb5094b95f694d8
Ratta38 <k024g1030@g.neec.ac.jp>
2026-05-18 14:33:19 +0900

    a

--- DirectXGame/AppSource/GameObject/GPUParticle/GPUPRender.cpp
+++ DirectXGame/AppSource/GameObject/GPUParticle/GPUPRender.cpp
@@ -3,5 +3,5 @@
 GPUP::RenderBufferData GPUP::Render::Initialize(uint32_t num, const SHEngine::DrawData& dd) {
 	render_ = std::make_unique<SHEngine::Renderer>(dd);
 	container_ = std::make_unique<SHEngine::BufferContainer>();
-
+	return {};
 }
