d57b7f640b9d5b276e5d6eceeb9e3d64da8c8bff
ShigemoriMasato <k024g0027@g.neec.ac.jp>
2026-05-28 16:02:13 +0900

    assets整理

---
+++ DirectXGame/AppSource/Scene/01_Title/TitleLogo/Compute.cpp
@@ -0,0 +1,7 @@
+#include "Compute.h"
+
+void Compute::Initialize(SHEngine::Engine* engine) {
+}
+
+void Compute::Update(float deltaTime, Camera* camera) {
+}
---
+++ DirectXGame/AppSource/Scene/01_Title/TitleLogo/Compute.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <SHEngine.h>
+#include <Camera/Camera.h>
+#include <Render/Renderer.h>
+
+class Compute {
+public:
+
+	void Initialize(SHEngine::Engine* engine);
+	void Update(float deltaTime, Camera* camera);
+	void Draw(SHEngine::Screen::IDisplay* disp);
+
+private:
+
+	std::unique_ptr<CmdObj> compute_;
+	std::unique_ptr<CmdObj> direct_;
+
+};
